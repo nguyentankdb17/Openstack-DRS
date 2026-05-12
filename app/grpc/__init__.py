c77d3b7ca2a7563aa20f7e53e5d7ff8e52d772b6
nguyentankdb17 <nguyentankdb17@gmail.com>
2026-05-12 16:20:02 +0000

    feat: add gRPC service stubs and generated code for analytics, collector, engine, and scoring modules

---
+++ app/grpc/__init__.py
@@ -0,0 +1,11 @@
+"""
+Generated stubs and helpers for gRPC services.
+To regenerate after modifying .proto files:
+
+python -m grpc_tools.protoc \
+  -I protos \
+  --python_out=app/grpc \
+  --grpc_python_out=app/grpc \
+  protos/*.proto
+
+"""
